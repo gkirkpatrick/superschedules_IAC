e9363b2611c1993000e7edab23add18c5d927f79
Greg Kirkpatrick <gkirkpatrick@users.noreply.github.com>
2025-08-28 14:35:02 -0400

    Add navigator setup script

--- terraform/dev/scripts/setup_superschedules_navigator.sh
+++ terraform/dev/scripts/setup_superschedules_navigator.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-REPO_DIR="$HOME/superschedules_collector"
+REPO_DIR="$HOME/superschedules_navigator"
 if [ ! -d "$REPO_DIR" ]; then
-  git clone git@github.com:gkirkpatrick/superschedules_collector "$REPO_DIR"
+  git clone git@github.com:gkirkpatrick/superschedules_navigator "$REPO_DIR"
 else
   (cd "$REPO_DIR" && git pull)
 fi
 if [ ! -d "$REPO_DIR/.venv" ]; then
-  python -m venv "$REPO_DIR/.venv" --prompt "superschedules_collector"
+  python -m venv "$REPO_DIR/.venv" --prompt "superschedules_navigator"
 fi
 source "$REPO_DIR/.venv/bin/activate"
 pip install -r "$REPO_DIR/requirements.txt"
